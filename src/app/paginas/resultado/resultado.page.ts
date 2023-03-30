a7bafb6d5e81f6f17320bcd3567bb385f2beab36
Flavia Sena <flaviatecnologia2023@gmail.com.br>
2023-03-30 19:53:56 -0300

    alteração

--- src/app/paginas/resultado/resultado.page.ts
+++ src/app/paginas/resultado/resultado.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+
 @Component({
   selector: 'app-resultado',
   templateUrl: './resultado.page.html',
@@ -36,3 +37,4 @@ export class ResultadoPage implements OnInit {
   
 
 }
+
